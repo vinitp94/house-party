20eaefdfafe7bc66ab05ba408f3893cdbf6022fa
Vinit Parikh <vinitp94@gmail.com>
2020-04-25 20:31:52 -0700

    anotha one

--- src/components/building-blocks/dance-floor.js
+++ src/components/building-blocks/dance-floor.js
@@ -9,7 +9,7 @@ export default function DanceFloor() {
       <div className="banner">
         DANCE FLOOR - Add some bangers to the playlist!
       </div>
-      <a target="_blank" href="https://meet.jit.si/ProjectAkashDanceFloor">
+      <a target="_blank" href="https://duke.zoom.us/j/99458121820">
         <div className="action-btn">Enter</div>
       </a>
       <a target="_blank" href="https://open.spotify.com/playlist/5LFWI8SdtzlE9JDCWLbsDw?si=dihRkHznRg-ipiZC8UHB9A">
